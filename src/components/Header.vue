a2e1c6ab723f7fbd3be2e462ba65d7c99a2b4856
Laís <87990551+sanolilais@users.noreply.github.com>
2023-07-24 17:16:32 -0300

    feat: added simple header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,26 @@
+<script setup>
+import NavBar from './NavBar.vue';
+</script>
+
 <template>
     <main>
         <header>
             <div class="header-items">
-                <h1>Oi</h1>
+                <NavBar />
             </div>
         </header>
     </main>
 </template>
+
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Libre+Baskerville&family=Ubuntu:wght@400;700&display=swap');
+
+.header-items {
+    background-color: #fae1fa;
+    height: 170px;
+    display: flex;
+    font-family: Ubuntu, Arial, Helvetica, sans-serif;
+    font-weight: 700;
+}
+
+</style>
