b17de4a74e32341cecff3ba4f5dacc9bcbff7238
Laís <87990551+sanolilais@users.noreply.github.com>
2023-07-27 07:20:01 -0300

    feat: selected font family and navbar text size/weight changes

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -13,6 +13,8 @@
 </template>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Merriweather+Sans:wght@800&family=Poppins:wght@500&family=Signika+Negative:wght@500;700&display=swap');
+
 .navbar-items {
     display: flex;
     align-items: center;
@@ -20,10 +22,16 @@
 }
 
 .navbar-items__logo-site {
+    font-family: 'Merriweather Sans' , sans-serif;
+    font-size: 22px;
+    font-weight: 800;
     color: #952194;
 }
 
 .navbar-items__links__menu {
+    font-family: 'Signika Negative', sans-serif;
+    font-size: 18px; 
+    font-weight: 700;
     display: flex;
     list-style: none;
     gap: 30px;
